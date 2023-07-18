445819fa30a8145f824b281094b42581023ab8dc
Henry Schreiner <henryschreineriii@gmail.com>
2023-07-17 23:47:49 -0400

    chore: update & add lock
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- check.py
+++ check.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
+import importlib.util
 import sys
 
-try:
-    assert sys.version_info >= (3, 7), "Python should be 3.7+"
+assert sys.version_info >= (3, 8), "Python should be 3.8+"
 
-    import pandas
-    import numba
-    import line_profiler
-except (AssertionError, ImportError) as e:
-    print("Environment not setup property! Use conda, see readme.")
-    print("Maybe `conda activate performance-minicourse` is missing?")
-    print(e)
-    sys.exit(1)
+
+for lib in ["pandas", "numba", "line_profiler"]:
+    if importlib.util.find_spec(lib) is None:
+        print("Environment not setup property! Use conda, see readme.")
+        print("Maybe `conda activate performance-minicourse` is missing?")
+        print("At least", lib, "is missing")
+        sys.exit(1)
 
 print("Environment appears correct, congratulations!")
